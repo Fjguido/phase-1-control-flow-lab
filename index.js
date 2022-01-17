d7c363a9abd6197d46ee5401e891233672970341
Francis Guido <fjguido97@gmail.com>
2022-01-16 22:58:07 -0500

    commit tree dfa18e22ad2cba103ad40c872925096a698a3cc9

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+// function scuberGreetingForFeet(distance){
+//   if (distance <= 400){
+//     return 'This one is on me!'
+//   } else if ((distance > 2000) && (distance < 2500)){
+//     return 'I will gladly take your thirty bucks.'
+//   } else if (distance > 2500){
+//     return 'No can do.'
+//   }
+// }
+
+// function ternaryCheckCity(city){
+//    if (city === "NYC"){
+//      return "Ok, sounds good."
+//    } else {
+//      return "No go."
+//    }
+//   return (city === "NYC") ? "Ok, sounds good." : "No go."
+// }
+
+// function switchOnCharmFromTip(tipString){
+//    if (tipString === "generous"){
+//      return "Thank you so much."
+//    } else if (tipString === "not as generous"){
+//      return "Thank you."
+//    } else if (tipString === "thanks for everything"){
+//     return "Bye."
+//    }
+
+//   switch(tipString){
+//     case "generous":
+//       return "Thank you so much."
+//     case "not as generous":
+//       return "Thank you."
+//     case "thanks for everything":
+//       return "Bye."
+//   }
+// }
+
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return `This one is on me!`;
+  } else if((distance > 2000) && (distance <2500)){
+    return `I will gladly take your thirty bucks.`;
+  } else if(distance > 2500){
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+
+  if (city === `NYC`){
+    return `Ok, sounds good.`
+  } else{
+    return `No go.`
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString) {
+ switch(tipString){
+  case `generous`:
+    return `Thank you so much.`
+  case `not as generous`:
+    return `Thank you.`
+  case `thanks for everything`:
+    return `Bye.`
+  }
 }
